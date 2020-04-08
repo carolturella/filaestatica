078dec317b47a22d5000fca4d90cd4a6a9d0a6ff
carolturella <carolturella@globo.com>
2020-04-07 21:54:19 -0300

    criação clase app, lista e no

---
+++ src/main/java/br/turella/App.java
@@ -0,0 +1,27 @@
+package br.turella;
+
+public final class App {
+    private App() {
+    }
+
+
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+
+        lista.addEnd("Huguinho");
+        lista.addEnd("Zezinho");
+        lista.addEnd("Luizinho");
+        lista.addStart("Patinhas");
+        System.out.println(lista.show());
+
+        No removido = lista.removeStart();
+
+        System.out.println("Removido " + removido.getInfo());
+        System.out.println(lista.show());
+
+        removido = lista.removeEnd();
+        System.out.println("Removido" + removido.getInfo());
+        System.out.println(lista.show());
+
+    }
+}
---
+++ src/main/java/br/turella/No.java
@@ -0,0 +1,23 @@
+package br.turella;
+
+public class No {
+    private String info;
+    private No prox;
+
+    public No(String name){
+        info = name;
+        prox = null;
+    }
+
+    public String getInfo(){
+        return info;
+    }
+
+    public No getProx(){
+        return prox;
+    }
+
+    public void setProx(No no){
+        prox = no;
+    }
+}
